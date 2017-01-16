faa833590b83d7b29ca47758871dc2b366f42a99
Julian Bath <2bathj70@W65141.solent.ac.uk>
2017-01-16 12:36:39 +0000

    Added simple paintball firing thing

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject paintballPrefab;
+    public float shootForce;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject paintball = Instantiate(paintballPrefab);
+            paintball.transform.position = transform.FindChild("Spawn Point").position;
+            paintball.GetComponent<Rigidbody>().AddForce(transform.up * shootForce);
+            Destroy(paintball, 2f);
+        }
+    }
+}
