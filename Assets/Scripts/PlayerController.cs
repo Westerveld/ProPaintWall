739074799ab61250f4be9670f8b1b25cc47fddb3
Ethan Bruins <brudut93@gmail.com>
2017-01-16 12:07:48 +0000

    AddedTestNetworking Scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour {
+
+	void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
+
+        transform.Rotate(x, 0, 0);
+        transform.Translate(0, 0, z);
+
+    }
+}
