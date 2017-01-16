6caf7c8f9c4bf064bac757f3caac1d4f6700de0f
Julian Bath <2bathj70@W65141.solent.ac.uk>
2017-01-16 14:05:46 +0000

    Added main menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject mainMenu, optionsMenu;
+
+    public void ShowMain()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("NetworkLobby");
+    }
+
+    public void ShowOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
