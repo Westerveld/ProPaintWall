bf393f1e417d5bf9d63fb14e5f7c0c89fcbfbe09
Ethan Bruins <brudut93@gmail.com>
2017-01-18 15:41:02 +0000

    Gun and Cow Mayhem

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -4,17 +4,25 @@ using UnityEngine.Networking;
 public class GunController : NetworkBehaviour
 {
     public GameObject paintballPrefab;
+    private float lastTime, interval;
+    private int ammo;
+
+    void Start()
+    {
+        ammo = 30;
+        lastTime = 0;
+    }
 
     void Update()
     {
        
-            if (Input.GetButtonDown("Fire1"))
-            {
-                print("Fire");
-                CmdFirePaintBall();
-            }
-        
-
+        if (Input.GetButtonDown("Fire1") && ammo > 0 && Time.time > lastTime + interval)
+        {
+            print("Fire");
+            CmdFirePaintBall();
+            ammo--;
+            lastTime = Time.time;
+        }
     }
 
    [Command]
@@ -23,5 +31,10 @@ public class GunController : NetworkBehaviour
         print("CMDFIre");
         GameObject go =  (GameObject)Instantiate(paintballPrefab, transform.FindChild("Spawn Point").position, Quaternion.identity);
         NetworkServer.Spawn(go);
+    }
+
+    void AmmoRefill()
+    {
+        ammo = 30;
     }
 }
