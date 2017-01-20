759e083b84e6e7758def4817a56e2c8e9baab6a1
Julian Bath <2bathj70@W65141.solent.ac.uk>
2017-01-20 10:56:19 +0000

    Added ammo UI

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    public GameObject player;
+    public Text ammoText;
+
+    void Awake()
+    {
+        if (!player)
+        {
+            player = transform.parent.gameObject;
+        }
+
+        if (player.GetComponent<NetworkPlayer>().isLocalPlayer)
+        {
+            GetComponent<Canvas>().enabled = false;
+        }
+        else
+        {
+            GetComponent<Canvas>().enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        GunController gunController = player.GetComponent<GunController>();
+        if (gunController)
+        {
+            ammoText.text = "Ammo: " + gunController.ammo;
+        }
+    }
+}
